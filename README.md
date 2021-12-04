eb7313d84f962527634b0044962e89765ad867a7
Harris Marfel <hrace009@gmail.com>
2021-12-04 15:52:38 +0700

    First Release

---
+++ README.md
@@ -0,0 +1,5 @@
+## Perfect World API
+
+Original project by [Tom Irons](https://github.com/tomirons/pw-api).
+
+This package still testing please don't use on production server!
---
+++ src/hrace009/PerfectWorldAPI/ServiceProvider.php
@@ -0,0 +1,51 @@
+<?php
+/*
+ * @author Harris Marfel <hrace009@gmail.com>
+ * @link https://www.hrace009.com
+ * @copyright Copyright (c) 2021.
+ */
+
+namespace hrace009\PerfectWorldAPI;
+
+use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->publishFiles();
+    }
+
+    /**
+     * Publish files for the package.
+     *
+     * @return void
+     */
+    protected function publishFiles()
+    {
+        $this->publishes([
+            __DIR__ . '/../../config/pw-api.php' => config_path('pw-api.php'),
+        ]);
+
+        $this->publishes([
+            __DIR__ . '/../../lang' => base_path('resources/lang'),
+        ]);
+    }
+
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+}
